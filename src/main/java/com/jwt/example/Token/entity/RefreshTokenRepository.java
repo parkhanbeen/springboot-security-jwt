c48ddd6556eca89558e161fdd2ee4c549097318d
phb0228 <phb0228@naver.com>
2021-09-10 18:12:23 +0900

    리프레시 토큰 관련 엔티티를 추가하라

---
+++ src/main/java/com/jwt/example/Token/entity/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.jwt.example.Token.entity;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+  Optional<RefreshToken> findByKey(String key);
+}
