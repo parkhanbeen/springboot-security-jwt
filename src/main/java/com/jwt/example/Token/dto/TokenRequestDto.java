e8fe156089f14f4ccd2256a48789e8f164e05682
phb0228 <phb0228@naver.com>
2021-09-10 18:12:48 +0900

    토큰 관련 dto를 추가하라

---
+++ src/main/java/com/jwt/example/Token/dto/TokenRequestDto.java
@@ -0,0 +1,18 @@
+package com.jwt.example.Token.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TokenRequestDto {
+  private String accessToken;
+  private String refreshToken;
+
+  @Builder
+  public TokenRequestDto(String accessToken, String refreshToken) {
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+  }
+}
